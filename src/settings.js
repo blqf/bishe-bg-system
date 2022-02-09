8b4bca5a14e4cf57709805cba460a700922167c3
milk054 <2022108200@qq.com>
2022-02-09 15:14:34 +0800

    fix: 修改网站title

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "北极人后台管理系统",
+  title: "服装后台管理系统",
 
   /**
    * @type {boolean} true | false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,7 +9,7 @@
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">北极人后台管理系统</h3>
+        <h3 class="title">服装后台管理系统</h3>
       </div>
 
       <!-- 用户名 -->
